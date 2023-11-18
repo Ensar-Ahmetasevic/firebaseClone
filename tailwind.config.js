e864433fe7f1c799d4f2d338b74201512ff15bef
Ensar Ahmetasevic <111744786+Ensar-Ahmetasevic@users.noreply.github.com>
2023-11-18 17:59:38 +0100

    installing react icons and prettier-plugin-tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-opensans)"],
       },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1020px",
+        xl: "1440px",
+      },
+      colors: {
+        navbarBgColor: "hsl(216.77,89.86%,72.94%)",
+        navbar2Color: "hsl(200,81.82%,93.53%)",
+        buttonBlue: "hsl(214.7,78.72%,46.08%)",
+        textColor: "hsl(0,0%,25.88%)",
+        PageBgColor: "hsl(214.08,81.75%,50.59%)",
+      },
+      // backgroundImage: () => ({
+      //   dots: "url('/bg-dots.svg')",
+      // }),
     },
   },
   plugins: [],
-}
+};
